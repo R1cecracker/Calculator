65158ffefc4a88777b35b727b85c7eae0295dbe5
Troy Harcoan <troy.harcoan0@gmail.com>
2024-11-13 13:26:44 +1100

    //added grades to calculator

---
+++ calculator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main() {
+    int mark = 50;
+    switch (mark) {
+        case 85 ... 100:
+            printf("High Distinction\n");
+            break;
+
+
+        case 75 ... 84:
+            printf("Distinction\n");
+            break;
+
+        case 65 ... 74:
+            printf("Credit\n");
+            break;
+            
+
+        case 50 ... 64:
+            printf("Pass\n");
+            break;
+
+        
+        default:
+            printf("FAIL\n");
+        
+    }
+}
